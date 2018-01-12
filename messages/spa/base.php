4b00acb0fd3aca19e40bb6a323ad76b51325212c
César A. Vargas-Pérez <ca.vargas28@gmail.com>
2018-01-12 08:17:35 -0400

    Create base.php
    Spanish translation

---
+++ messages/spa/base.php
@@ -0,0 +1,7 @@
+<?php
+    return [
+        'ChatWee Settings' => 'Ajustes de ChatWee',
+        'ChatWee Widget URL:' => 'ChatWee Widget URL:',
+        '<strong>ChatWee</strong> module configuration' => 'Configuración del módulo de <strong>ChatWee</strong>',
+        'Save' => 'Guardar',
+    ];
